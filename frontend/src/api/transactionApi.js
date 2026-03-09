fd38261a6260de039eec57d1205264bed39da418
Lovish <lovishgupta354@gmail.com>
2026-03-09 21:28:34 +0530

    fix api base

--- frontend/src/api/transactionApi.js
+++ frontend/src/api/transactionApi.js
@@ -4,7 +4,7 @@
  */
 import axios from 'axios';
 
-const API_BASE = 'https://expense-backend-dkwv.onrender.com/api'
+const API_BASE = import.meta.env.VITA_API_URL
 
 export const transactionApi = {
   // GET all transactions
